95083450a1e17ffd3609a8e70830524a9a18fa48
zhanghaoxin <zhanghaoxin@corp.netease.com>
2019-02-20 19:38:38 +0800

    修改依赖

--- setup.py
+++ setup.py
@@ -5,10 +5,13 @@ from setuptools import find_packages
 
 setup(
         name='common-tool',     # 包名字
-        version='1.0.0.dev3',   # 包版本
+        version='1.0.0.dev4',   # 包版本
         description='This is a test of the setup',   # 简单描述
         author='zhxhash',  # 作者
         author_email='zhxhash@vip.sina.com',  # 作者邮箱
         url='https://https://github.com/HashZhang/common-tool',      # 包的主页
         packages=find_packages(where='.', exclude=(), include=('*',)),                 # 包，搜索所有的包含__init__.py的目录
+        install_requires=[
+                'redis'
+        ]
 )
