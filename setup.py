937ea850ac95d2839e1afa688e91f6326eb4b14f
zhanghaoxin <zhanghaoxin@corp.netease.com>
2019-02-20 10:21:19 +0800

    增加setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+# or
+# from distutils.core import setup
+
+setup(
+        name='common-tool',     # 包名字
+        version='1.0.0-snapshot',   # 包版本
+        description='This is a test of the setup',   # 简单描述
+        author='zhxhash',  # 作者
+        author_email='zhxhash@vip.sina.com',  # 作者邮箱
+        url='https://https://github.com/HashZhang/common-tool',      # 包的主页
+        packages=['demo'],                 # 包
+)
