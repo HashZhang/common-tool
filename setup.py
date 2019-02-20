9a52b16de55898b7d830d8a0b134481a844194f2
zhanghaoxin <zhanghaoxin@corp.netease.com>
2019-02-20 19:32:25 +0800

    修改版本

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 setup(
         name='common-tool',     # 包名字
-        version='1.0.0.dev1',   # 包版本
+        version='1.0.0.dev2',   # 包版本
         description='This is a test of the setup',   # 简单描述
         author='zhxhash',  # 作者
         author_email='zhxhash@vip.sina.com',  # 作者邮箱
         url='https://https://github.com/HashZhang/common-tool',      # 包的主页
-        packages=['.'],                 # 包
+        packages=['common'],                 # 包
 )
